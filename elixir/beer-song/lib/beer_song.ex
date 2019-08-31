c35e271ffb7711258e277f8cc8f14ec3a9f0cdcd
Jesus Dugarte <jdugarte@gmail.com>
2019-08-30 20:23:24 -0400

    use pattern matching in the verse function

--- elixir/beer-song/lib/beer_song.ex
+++ elixir/beer-song/lib/beer_song.ex
@@ -3,27 +3,41 @@ defmodule BeerSong do
   @doc """
   Get a single verse of the beer song
   """
-  @spec verse(integer) :: String.t
-  def verse(number) do
+  @spec verse(integer) :: String.t()
+  def verse(0) do
     """
-    #{ String.capitalize bottles(number) } of beer on the wall, #{ bottles number } of beer.
-    #{ take_or_buy number }, #{ bottles number-1 } of beer on the wall.
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
     """
   end
 
-  defp bottles(0), do: "no more bottles"
-  defp bottles(1), do: "1 bottle"
-  defp bottles(-1), do: bottles 99
-  defp bottles(count), do: "#{count} bottles"
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
 
-  defp take_or_buy(0), do: "Go to the store and buy some more"
-  defp take_or_buy(1), do: "Take it down and pass it around"
-  defp take_or_buy(_), do: "Take one down and pass it around"
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  def verse(number) do
+    """
+    #{bottles(number)} of beer on the wall, #{bottles(number)} of beer.
+    Take one down and pass it around, #{bottles(number - 1)} of beer on the wall.
+    """
+  end
+
+  defp bottles(count), do: "#{count} bottles"
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
-  @spec lyrics(Range.t) :: String.t
-  def lyrics(range \\ 99..0), do: Enum.map_join range, "\n", &verse/1
+  @spec lyrics(Range.t()) :: String.t()
+  def lyrics(range \\ 99..0), do: Enum.map_join(range, "\n", &verse/1)
 
 end
