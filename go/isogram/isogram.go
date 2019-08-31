84224a26c9d7dc5f5e4ad8e1be280f7d69897fb4
Jesus Dugarte <jdugarte@gmail.com>
2019-08-31 12:16:45 -0400

    flattening conditions

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -13,12 +13,13 @@ func IsIsogram(input string) bool {
 	chars := register{}
 	for _, char := range input {
 		char = unicode.ToLower(char)
-		if unicode.IsLetter(char) {
-			if _, exists := chars[char]; exists {
-				return false
-			}
-			chars[char] = checked{}
+		if !unicode.IsLetter(char) {
+			continue
 		}
+		if _, exists := chars[char]; exists {
+			return false
+		}
+		chars[char] = checked{}
 	}
 	return true
 }
