f143bca07d4aa20010621c46422a73c2166dce19
Jesus Dugarte <jdugarte@gmail.com>
2019-08-31 13:16:46 -0400

    update the exercise to the latest version

--- elixir/robot-simulator/lib/robot_simulator.ex
+++ elixir/robot-simulator/lib/robot_simulator.ex
@@ -7,10 +7,10 @@ defmodule RobotSimulator do
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
-  @spec create(direction :: atom, position :: { integer, integer }) :: any
-  def create(direction \\ :north, position \\ { 0, 0 })
+  @spec create(direction :: atom, position :: {integer, integer}) :: any
+  def create(direction \\ :north, position \\ {0, 0})
   def create(direction, _) when direction not in @cardinal_directions, do: { :error, "invalid direction" }
-  def create(direction, position = { x, y }) when is_number(x) and is_number(y) do
+  def create(direction, position = {x, y}) when is_number(x) and is_number(y) do
     %{ direction: direction, position: position }
   end
   def create(_, _), do: { :error, "invalid position" }
@@ -20,32 +20,35 @@ defmodule RobotSimulator do
 
   Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
   """
-  @spec simulate(robot :: any, instructions :: String.t ) :: any
+  @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, ""), do: robot
   def simulate(robot, <<next::binary-1, tail::binary>>) when next in ~w[ L R A ] do
-    simulate (if next == "A", do: advance(robot), else: change_direction(robot, next)), tail
+    simulate(if(next == "A", do: advance(robot), else: change_direction(robot, next)), tail)
   end
   def simulate(_, _), do: { :error, "invalid instruction" }
 
   @right @cardinal_directions |> List.duplicate(2) |> List.flatten
-  @left  @right |> Enum.reverse
+  @left @right |> Enum.reverse
   @directions %{ "L" => @left, "R" => @right }
 
   defp change_direction(robot, instruction) do
-    Map.update! robot, :direction, fn direction ->
-      Enum.at @directions[instruction], Enum.find_index(@directions[instruction], &(&1 == direction)) + 1
-    end
+    Map.update!(robot, :direction, fn direction ->
+      Enum.at(
+        @directions[instruction],
+        Enum.find_index(@directions[instruction], &(&1 == direction)) + 1
+      )
+    end)
   end
 
   defp advance(robot) do
-    Map.update! robot, :position, fn { x, y } ->
+    Map.update!(robot, :position, fn {x, y} ->
       case direction(robot) do
-        :north -> { x, y+1 }
-        :south -> { x, y-1 }
-        :east  -> { x+1, y }
-        :west  -> { x-1, y }
+        :north -> { x, y + 1 }
+        :south -> { x, y - 1 }
+        :east  -> { x + 1, y }
+        :west  -> { x - 1, y }
       end
-    end
+    end)
   end
 
   @doc """
@@ -59,7 +62,7 @@ defmodule RobotSimulator do
   @doc """
   Return the robot's position.
   """
-  @spec position(robot :: any) :: { integer, integer }
+  @spec position(robot :: any) :: {integer, integer}
   def position(robot), do: robot[:position]
 
 end
