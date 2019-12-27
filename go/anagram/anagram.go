196e9fe3ab0ec4e7734d46c78ff198cdbfe5787d
Jesus Dugarte <jdugarte@gmail.com>
2019-12-26 20:55:52 -0500

    don't use append

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -8,13 +8,16 @@ import (
 
 // Detect returns a correct list of anagrams, given a word and a list of possible anagrams
 func Detect(subject string, candidates []string) (result []string) {
+	var i int
+	result = make([]string, len(candidates))
 	encoded := encode(subject)
 	for _, word := range candidates {
 		if strings.ToLower(subject) != strings.ToLower(word) && encoded == encode(word) {
-			result = append(result, word)
+			result[i] = word
+			i++
 		}
 	}
-	return result
+	return result[:i]
 }
 
 func encode(word string) string {
