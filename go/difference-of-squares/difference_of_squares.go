bb3baa8534464ab5f48a7ef1b6a1a6e48fbdde76
Jesus Dugarte <jdugarte@gmail.com>
2020-02-21 06:23:20 -0500

    remove math

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,22 +1,19 @@
 package diffsquares
 
-import "math"
-
 // SquareOfSum returns the square of the sum of the first N natural numbers
 func SquareOfSum(n int) (sum int) {
 	for i := 1; i <= n; i++ {
 		sum += i
 	}
-	return int(math.Pow(float64(sum), 2))
+	return sum * sum
 }
 
 // SumOfSquares returns the sum of the squares of the first N natural numbers
-func SumOfSquares(n int) int {
-	var sum float64
+func SumOfSquares(n int) (sum int) {
 	for i := 1; i <= n; i++ {
-		sum += math.Pow(float64(i), 2)
+		sum += i * i
 	}
-	return int(sum)
+	return sum
 }
 
 // Difference returns the difference between the square of the sum and the sum of the squares of the first N natural numbers
